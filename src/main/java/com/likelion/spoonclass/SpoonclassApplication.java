9a0b9a4c520d439b45ea450d69258de5b9ab4e48
gyedaehwan_desktop <daehwan7989@naver.com>
2021-11-15 00:58:52 +0900

    🌞[INIT]-#1-spring INIT
    ## 개요
    Spring 초기 개발 환경 세팅
    1. Dependencies 추가
    2. .gitignore 에 yml 관련 추가

---
+++ src/main/java/com/likelion/spoonclass/SpoonclassApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.spoonclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoonclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoonclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/spoonclass/SpoonclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.spoonclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoonclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
