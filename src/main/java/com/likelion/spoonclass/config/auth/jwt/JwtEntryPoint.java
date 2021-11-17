d9e2a66925a442b741f7c3766e07ef91a0e8f094
gyedaehwan <daehwan7989@naver.com>
2021-11-17 12:36:32 +0900

    🔧[FIX]-#1-yml 설정
    # 커밋 내용 (한 줄 72자 내)
    
    datasource 연동

--- src/main/java/com/likelion/spoonclass/config/auth/jwt/JwtEntryPoint.java
+++ src/main/java/com/likelion/spoonclass/config/auth/jwt/JwtEntryPoint.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component()
+@Component
 @RequiredArgsConstructor
 public class JwtEntryPoint implements AuthenticationEntryPoint {
     private final JwtProvider jwtProvider;
