17fbd20c5e125aa364989d00f6b7f8a2eee3a7eb
gyedaehwan_desktop <daehwan7989@naver.com>
2021-11-22 19:34:40 +0900

    🔧[FIX]-#29-build.gradle 수정
    ## 개요
    - 내용을 적어주세요.

--- src/test/java/com/likelion/spoonclass/common/BaseTest.java
+++ src/test/java/com/likelion/spoonclass/common/BaseTest.java
@@ -1,10 +1,13 @@
 package com.likelion.spoonclass.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -12,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureRestDocs(uriScheme = "https",
         uriHost = "docs.api.com",
         outputDir = "build/generated-snippets")
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 public abstract class BaseTest {
     @Autowired
     protected MockMvc mockMvc;
