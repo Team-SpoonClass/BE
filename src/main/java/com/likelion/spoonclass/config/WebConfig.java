7ba7474138a4d22ea2ac51281786f673a10fc1cc
gyedaehwan_desktop <daehwan7989@naver.com>
2021-11-29 01:20:05 +0900

    CORS
    ## 개요
    - 내용을 적어주세요.
    
    ## 작업사항
    - 내용을 적어주세요.
    
    ## 변경로직
    - 내용을 적어주세요.
    
    #### 테스트인 경우 이하 내용 작성 ####
    # 테스트 내용 (한 줄 72자 내)
    
    *어떤것을 테스트했고, 테스트 성공 여부 작성*
    
    #### (이상) ####
    
    # CREATE _ yellow
    🌞[INIT] : 초기 생성
    🔥[FEAT] : 새 기능 구현
    📁[LIB]  : 라이브러리 추가
    
    # MODIFY _ white
    📈[UPDATE] : 코드 수정 및 향상 (오류 수정과 다르다)
    🔧[FIX]    : 버그 및 오류 수정, 해결
    💨[MOVE]   : 프로젝트 내 파일 주소 변경 시 
         (* 모든 팀원과 상의 후 높은 브랜치에서 진행)
    🖍[RENAME] : 프로젝트 내 파일 명 변경 시 
         (* 모든 팀원과 상의 후 높은 브랜치에서 진행)
    🌪️[REF]: 코드 리팩토링 
         (코드 전면 수정 시, 모든 팀원과 상의 후 브랜치 충돌에 주의해서 진행)
    
    # REMOVE _ black
    🌚[CLEAR]  : 코드 삭제
    🚥[TIDY]   : 중복되는 코드, 기능과 관련없는 코드 등 정리
    💣[REMOVE] : 파일 삭제 (* 모든 팀원과 상의 후 높은 브랜치에서 진행)
    
    # TEST 
    📖[TEST]   : 테스트 코드 생성
    
    # GIT
    🔀[MERGE] : 기타 브랜치 머지
    
    # ELSE
    ⚠️[EMG]  : 긴급한 작업 수행 
    📜[DOCS] : 문서 작업 
         (README, RESTDOCS 등, 프로젝트 스코프에 맞는 브랜치에서 진행)

--- src/main/java/com/likelion/spoonclass/config/WebConfig.java
+++ src/main/java/com/likelion/spoonclass/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-/*
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -14,5 +13,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .maxAge(3600)
                 .allowCredentials(true);
-    }*/
+    }
 }
